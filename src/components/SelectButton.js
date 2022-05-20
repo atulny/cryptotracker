1c8d49b0829afebb3d0fc6772ea0e355c0e6ef16
Atul <atulny@gmail.com>
2022-05-19 20:18:41 -0400

    init

---
+++ src/components/SelectButton.js
@@ -0,0 +1,34 @@
+import { makeStyles } from "@material-ui/core";
+
+const SelectButton = ({ children, selected, onClick }) => {
+  const useStyles = makeStyles({
+    selectbutton: {
+      border: "1px solid rgb(169 197 245)",
+      borderRadius: 5,
+      padding: 10,
+      paddingLeft: 20,
+      paddingRight: 20,
+      fontFamily: "Montserrat",
+      cursor: "pointer",
+      backgroundColor: selected ? "rgb(169 197 245)" : "",
+      color: selected ? "black" : "",
+      fontWeight: selected ? 700 : 500,
+      "&:hover": {
+        backgroundColor: "rgb(169 197 245)",
+        color: "black",
+      },
+      width: "22%",
+      //   margin: 5,
+    },
+  });
+
+  const classes = useStyles();
+
+  return (
+    <span onClick={onClick} className={classes.selectbutton}>
+      {children}
+    </span>
+  );
+};
+
+export default SelectButton;
